d2ab3899ec0fcfaac15ba79e468540532c4e9dde
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-06-09 18:24:14 -0400

    added pokemon class

--- pokemon_class.py
+++ pokemon_class.py
@@ -1,8 +1,18 @@
 from type_enum import Type
 
 class Pokemon:
-    def __init__(self, weight: float, type_one: Type, type_two: Type, ability: str):
-        self.weight = weight
+    def __init__(self, name: str, type_one: Type, type_two: Type | None, hp: int, phy_attack: int, phy_defense: int, spec_attack: int, spec_defense: int, speed: int, ability: str, weight: float) -> None:
+        self.name = name
         self.type_one = type_one
-        self.type_two = None
+        if type_two:
+            self.type_two = type_two
+        else:
+            self.type_two = None
+        self.hp = hp
+        self.phy_attack = phy_attack
+        self.phy_defense = phy_defense
+        self.spec_attack = spec_attack
+        self.spec_defense = spec_defense
+        self.speed = speed
         self.ability = ability
+        self.weight = weight
