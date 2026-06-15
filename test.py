9982ffdafc257ee268039b4cb608415b23e11541
Hoffinator3 <123334411+Hoffinator3@users.noreply.github.com>
2026-06-15 15:30:58 -0400

    Function to test class methods used during a battle

---
+++ test.py
@@ -0,0 +1,29 @@
+import sys
+import json
+import os
+from pokemon.pokemon import Pokemon
+from pokemon.moves import Moves
+#from pokemon.moves import Moves
+##from pokemon.battle import Battle
+#from trainers.player import Player
+#from trainers.trainer import Trainer
+
+
+def test_battle():
+    b = None
+    s = None
+    with open(os.path.abspath("./data/pokemon.json"), mode='r') as d:
+        pokemon = json.load(d)
+        b = Pokemon("Bulbasaur", 5, pokemon["bulbasaur"]["stats"], pokemon["bulbasaur"]["type"], [Moves("tackle")], None)
+        s = Pokemon("Squirtle", 5, pokemon["squirtle"]["stats"], pokemon["squirtle"]["type"], [Moves("tackle")], None)
+
+    s.moves[0].calculate_damage(s, b)
+
+    bulb_hp = pokemon["bulbasaur"]["stats"]["hp"]
+    b.take_damage(s.moves[0].calculate_damage(s, b))
+
+    print(f"- Bulbasaur\nOriginal HP: {bulb_hp}\nDamage take: {s.moves[0].calculate_damage(s, b)}\nRemaining HP: {b.stats["hp"]}")
+
+
+if __name__ == "__main__":
+    test_battle()
